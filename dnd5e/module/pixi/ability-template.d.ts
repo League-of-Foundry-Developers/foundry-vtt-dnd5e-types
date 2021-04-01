016dd4948028945e768fb78b7fc92ff28cecaa9a
David Archibald <david@davidarchibald.dev>
2021-04-01 00:37:02 -0600

    feat: fill in AbilityTemplate

--- dnd5e/module/pixi/ability-template.d.ts
+++ dnd5e/module/pixi/ability-template.d.ts
@@ -1,3 +1,31 @@
-declare class AbilityTemplate {}
+import Item5e from '../item/entity';
+
+/**
+ * A helper class for building MeasuredTemplates for 5e spells and abilities
+ * @extends {MeasuredTemplate}
+ */
+declare class AbilityTemplate {
+  /**
+   * A factory method to create an AbilityTemplate instance using provided data from an Item5e instance
+   * @param {Item5e} item               The Item object for which to construct the template
+   * @return {AbilityTemplate|null}     The template object, or null if the item does not produce a template
+   */
+  static fromItem(item: Item5e): AbilityTemplate | null;
+
+  /* -------------------------------------------- */
+
+  /**
+   * Creates a preview of the spell template
+   */
+  drawPreview(): void;
+
+  /* -------------------------------------------- */
+
+  /**
+   * Activate listeners for the template preview
+   * @param {CanvasLayer} initialLayer  The initially active CanvasLayer to re-activate after the workflow is complete
+   */
+  activatePreviewListeners(initialLayer: CanvasLayer): void;
+}
 
 export default AbilityTemplate;
