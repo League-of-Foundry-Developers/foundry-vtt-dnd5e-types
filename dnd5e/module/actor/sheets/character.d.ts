3ffbe11be12372971f4746e2e8d696d0f7452008
Luke Abby <david@davidarchibald.dev>
2021-04-06 23:31:47 -0600

    feat: Migrate to TSDoc (#2)

--- dnd5e/module/actor/sheets/character.d.ts
+++ dnd5e/module/actor/sheets/character.d.ts
@@ -1,3 +1,62 @@
-declare class ActorSheet5eCharacter {}
+import Item5e from '../../item/entity';
+
+import Actor5e from '../entity';
+
+declare class ActorSheet5eCharacter extends Actor5e {
+  /**
+   * Organize and classify Owned Items for Character sheets
+   */
+  private _prepareItems(data: this['data']): void;
+
+  /* -------------------------------------------- */
+
+  /**
+   * A helper method to establish the displayed preparation state for an item
+   * @param item - Item to toggle the state of
+   */
+  private _prepareItemToggleState(item: Item5e): void;
+
+  /* -------------------------------------------- */
+  /*  Event Listeners and Handlers
+  /* -------------------------------------------- */
+
+  /**
+   * Activate event listeners using the prepared sheet HTML
+   * @param html - The prepared HTML object ready to be rendered into the DOM
+   */
+  activateListeners(html: JQuery): void;
+
+  /* -------------------------------------------- */
+
+  /**
+   * Handle mouse click events for character sheet actions
+   * @param event - The originating click event
+   */
+  private _onSheetAction(event: MouseEvent): Roll;
+
+  /* -------------------------------------------- */
+
+  /**
+   * Handle toggling the state of an Owned Item within the Actor
+   * @param event - The triggering click event
+   */
+  private _onToggleItem(event: MouseEvent): ReturnType<Item5e['update']>;
+
+  /* -------------------------------------------- */
+
+  /**
+   * Take a short rest, calling the relevant function on the Actor instance
+   * @param event - The triggering click event
+   */
+  private _onShortRest(event: MouseEvent): Promise<ReturnType<Actor5e['longRest']>>;
+
+  /* -------------------------------------------- */
+
+  /**
+   * Take a long rest, calling the relevant function on the Actor instance
+   * @param event - The triggering click event
+   */
+  private _onLongRest(event: MouseEvent): Promise<ReturnType<Actor5e['shortRest']>>;
+}
 
 export default ActorSheet5eCharacter;
--- dnd5e/module/macros.d.ts
+++ dnd5e/module/macros.d.ts
@@ -5,9 +5,9 @@
 /**
  * Create a Macro from an Item drop.
  * Get an existing item macro if one exists, otherwise create a new one.
- * @param {Object} data     The dropped data
- * @param {number} slot     The hotbar slot to use
- * @returns {Promise}
+ * @param data - The dropped data
+ * @param slot - The hotbar slot to use
+ * @returns - whether the macro could be created
  */
 export declare function create5eMacro(data: Item.Data | Actor.Data, slot: number): Promise<boolean | undefined>;
 
@@ -16,7 +16,7 @@ export declare function create5eMacro(data: Item.Data | Actor.Data, slot: number
 /**
  * Create a Macro from an Item drop.
  * Get an existing item macro if one exists, otherwise create a new one.
- * @param {string} itemName
- * @return {Promise}
+ * @param itemName - the name of the item
+ * @returns The item macro
  */
-export declare function rollItemMacro(itemName: string): Roll;
+export declare function rollItemMacro(itemName: string): Promise<Roll>;
